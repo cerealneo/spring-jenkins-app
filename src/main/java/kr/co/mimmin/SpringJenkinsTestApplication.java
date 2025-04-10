8163bba51340bcc5d8be6002ec3e2c83f37792f3
minsung <dpsdy1@gmail.com>
2025-04-10 15:45:32 +0900

    add

---
+++ src/main/java/kr/co/mimmin/SpringJenkinsTestApplication.java
@@ -0,0 +1,13 @@
+package kr.co.mimmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJenkinsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/mimmin/SpringJenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.mimmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
